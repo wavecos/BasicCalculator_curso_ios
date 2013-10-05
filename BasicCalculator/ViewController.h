506c953a62e6f62a1fb184007b1b35af7e5acdd6
Alfredo Arias <alfre.arias@gmail.com>
2013-10-05 11:48:04 -0400

    Initial Commit

---
+++ BasicCalculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BasicCalculator
+//
+//  Created by Tekhne on 10/5/13.
+//  Copyright (c) 2013 Tekhne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BasicCalculatorTests/BasicCalculatorTests.m
@@ -0,0 +1,34 @@
+//
+//  BasicCalculatorTests.m
+//  BasicCalculatorTests
+//
+//  Created by Tekhne on 10/5/13.
+//  Copyright (c) 2013 Tekhne. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BasicCalculatorTests : XCTestCase
+
+@end
+
+@implementation BasicCalculatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
