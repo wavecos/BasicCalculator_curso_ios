bd6c1ec9facff4f499095d7af28fbf2062c17ce4
Alfredo Arias <alfre.arias@gmail.com>
2013-10-05 13:03:18 -0400

    Basic Calc

--- BasicCalculator/ViewController.h
+++ BasicCalculator/ViewController.h
@@ -8,6 +8,33 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+typedef enum{Plus, Minus, Multiply, Divide} CalcOperation;
+
+@interface ViewController : UIViewController {
+    NSString *storage;
+    CalcOperation operation;
+}
+
+@property (weak, nonatomic) IBOutlet UILabel *display;
+
+-(IBAction)button0:(id)sender;
+-(IBAction)button1:(id)sender;
+-(IBAction)button2:(id)sender;
+-(IBAction)button3:(id)sender;
+-(IBAction)button4:(id)sender;
+-(IBAction)button5:(id)sender;
+-(IBAction)button6:(id)sender;
+-(IBAction)button7:(id)sender;
+-(IBAction)button8:(id)sender;
+-(IBAction)button9:(id)sender;
+
+-(IBAction)buttonPlus:(id)sender;
+-(IBAction)buttonMinus:(id)sender;
+-(IBAction)buttonMultiply:(id)sender;
+-(IBAction)buttonDivide:(id)sender;
+
+-(IBAction)buttonClear:(id)sender;
+-(IBAction)buttonCalculate:(id)sender;
+
 
 @end
--- BasicCalculator/ViewController.m
+++ BasicCalculator/ViewController.m
@@ -14,6 +14,8 @@
 
 @implementation ViewController
 
+@synthesize display;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -25,5 +27,97 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+- (IBAction)button0:(id)sender {
+    display.text = [NSString stringWithFormat:@"%@0",display.text];
+}
+
+- (IBAction)button1:(id)sender {
+    display.text = [NSString stringWithFormat:@"%@1",display.text];
+}
+
+- (IBAction)button2:(id)sender {
+    display.text = [NSString stringWithFormat:@"%@2",display.text];
+}
+
+- (IBAction)button3:(id)sender {
+    display.text = [NSString stringWithFormat:@"%@3",display.text];
+}
+
+- (IBAction)button4:(id)sender {
+    display.text = [NSString stringWithFormat:@"%@4",display.text];
+}
+
+- (IBAction)button5:(id)sender {
+    display.text = [NSString stringWithFormat:@"%@5",display.text];
+}
+
+- (IBAction)button6:(id)sender {
+    display.text = [NSString stringWithFormat:@"%@6",display.text];
+}
+
+- (IBAction)button7:(id)sender {
+    display.text = [NSString stringWithFormat:@"%@7",display.text];
+}
+
+- (IBAction)button8:(id)sender {
+    display.text = [NSString stringWithFormat:@"%@8",display.text];
+}
+
+- (IBAction)button9:(id)sender {
+    display.text = [NSString stringWithFormat:@"%@9",display.text];
+}
+
+- (IBAction)buttonPlus:(id)sender {
+    operation = Plus;
+    storage = display.text;
+    display.text=@"";
+}
+
+- (IBAction)buttonMultiply:(id)sender {
+    operation = Multiply;
+    storage = display.text;
+    display.text = [NSString stringWithFormat:@"%@*", storage];
+    //display.text=@"";
+}
+
+- (IBAction)buttonDivide:(id)sender {
+    operation = Divide;
+    storage = display.text;
+    display.text=@"";
+}
+
+- (IBAction)buttonMinus:(id)sender {
+    operation = Minus;
+    storage = display.text;
+    display.text=@"";
+}
+
+- (IBAction)buttonClear:(id)sender {
+    display.text = @"";
+}
+
+- (IBAction)buttonCalculate:(id)sender {
+    NSString *val = display.text;
+    switch(operation) {
+        case Plus :
+            display.text= [NSString stringWithFormat:@"%qi",[val longLongValue]+[storage
+                                                                                 longLongValue]];
+            break;
+        case Minus:
+            display.text= [NSString stringWithFormat:@"%qi",[storage longLongValue]-[val
+                                                                                     longLongValue]];
+            break;
+        case Divide:
+            display.text= [NSString stringWithFormat:@"%qi",[storage longLongValue]/[val
+                                                                                     longLongValue]];
+            break;
+        case Multiply:
+            display.text= [NSString stringWithFormat:@"%qi",[val longLongValue]*[storage
+                                                                                 longLongValue]];
+        break; }
+}
+
 
 @end
